5308255bf065d2e780a402aa1f85deafc8d9add0
Skromlee <suppawat.kromlee@m.swu.ac.th>
2022-11-23 11:48:47 +0700

    update vecel json

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const path = require("path");
 const colors = require("colors");
-// const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 
